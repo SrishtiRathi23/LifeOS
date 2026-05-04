da5ed959314f4c80fe1d08e0f42c7102f17e54dc
Srishti Rathi <srishtirathi723@gmail.com>
2026-05-04 15:06:24 +0530

    Fix Render backend port binding

--- lifeos/backend/src/server.ts
+++ lifeos/backend/src/server.ts
@@ -5,6 +5,8 @@ import { app } from "./app.js";
 import { env } from "./utils/env.js";
 import { logger } from "./lib/logger.js";
 
-app.listen(env.PORT, () => {
-  logger.info(`LifeOS backend running on http://localhost:${env.PORT}`);
+const host = env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+
+app.listen(env.PORT, host, () => {
+  logger.info(`LifeOS backend running on http://${host}:${env.PORT}`);
 });
